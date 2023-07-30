4ad7bca50940f700d94b136946ed9838b26eec50
IT19003160 <it19003160@my.sliit.lk>
2023-07-30 19:36:00 +0530

    Mail

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { sendEmail } from "../../lib/email";
+
+export default async function handler(req, res) {
+  const { email, message, name } = req.body;
+  await sendEmail({
+    from: email,
+    to: "itzmepvk@gmail.com",
+    subject: name,
+    html: `<div><span>from: [${email}]</span> <p>${message}</p></div>`,
+  });
+
+  return res.status(200).json({ message: "Email sent successfully" });
 }
